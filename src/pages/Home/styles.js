358758c082d3397e52fcb1f074e0b7f4538591aa
Gabriel <gabrieldma27@gmail.com>
2022-04-26 10:09:31 -0300

    style: :art: Width screen

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components';
 
 export const Container = styled.div`
-    width: 100vw;
+    width: 100%;
     display: flex;
     flex-direction: row;
     justify-content: space-around;
